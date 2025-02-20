be4b27a9e30b0cb38547597d561a99a7de9d5198
ismael <joseismaelvargas@users.noreply.github.com>
2025-02-19 23:19:27 -0300

    Se agrego motion y swiper

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,42 +1,176 @@
 import"../components/css/carrusel.css"
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay ,Pagination} from 'swiper/modules'; 
+import imgresponsive from"/img/responsive.png"
+import enfoque from"/img/enfoque.png"
+import enfoque2 from"/img/enfoque2.png"
+import { useState } from "react";
+
 export const Carrusel=()=>{
+  // const ScroolSection=(id)=>{
+  //  const scroll=document.querySelector(id)
+  //  if(scroll){
+  //   scroll.scrollIntoView({ behavior: "smooth" });
+  //  }
+  
+const [animation,setAnimationKey]=useState(0)
  return(
     <>
 <section className="seccion">
+   <Swiper
+    modules={[ Navigation,Autoplay,Pagination]}
+    slidesPerView={1} 
+    spaceBetween={30} 
+    onSlideChange={() => setAnimationKey((prev) => prev + 1)}
+    autoplay={{
+      delay:5000
+  }}
+  pagination={{
+    clickable: true,
+  }}
+  loop={true}>
+      <SwiperSlide>
   <div className="container mx-auto text-center">
-    <h2 className="title-name text-4xl font-bold text-white">Hola, Soy Ismael.</h2>
-    <p className="subtitle text-lg text-gray-300 mt-2">
+  <motion.h2
+  key={`h2-${animation}`}
+initial={{ opacity: 0, x: 100 }} 
+animate={{ opacity: 1, x: 0 }}     
+  transition={{ duration: 1.5,delay: 0.5}}
+  className="title-name text-4xl font-bold text-white"
+>
+  Hola, Soy Ismael.
+</motion.h2>
+
+    <motion.p    key={`p-${animation}`}initial={{ opacity: 0, x:100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 1.5 ,delay: 0.5}} className="subtitle text-lg text-gray-300 mt-2">
       Programador y desarrollador web de Argentina. Especializado en el área del Frontend.
-    </p>
+    </motion.p>
 
 
   
 
-{/* 
-    <div className="logos-container flex justify-center gap-4 mt-6">
-      <img src="logo1.png" alt="Logo 1" className="h-12 w-12" />
-      <img src="logo2.png" alt="Logo 2" className="h-12 w-12" />
-      <img src="logo3.png" alt="Logo 3" className="h-12 w-12" />
-      <img src="logo4.png" alt="Logo 4" className="h-12 w-12" />
-    </div> */}
+
   </div>
 
 
   <picture className="imagenes-container mt-6">
     <source 
-      srcSet="https://tse4.mm.bing.net/th?id=OIP.NQvv9ODaTT8TWp3pI_d1WAHaHa&pid=Api&P=0&h=180" 
+      srcSet={enfoque2}
       media="(max-width:639px)" 
     />
     <source 
-      srcSet="https://tse4.mm.bing.net/th?id=OIP.NQvv9ODaTT8TWp3pI_d1WAHaHa&pid=Api&P=0&h=180" 
+      srcSet={enfoque2}
       media="(min-width:640px)" 
     />
-    <img 
-      className="img-main w-full max-w-sm mx-auto rounded-lg shadow-lg" 
-      src="https://tse4.mm.bing.net/th?id=OIP.NQvv9ODaTT8TWp3pI_d1WAHaHa&pid=Api&P=0&h=180" 
+    <motion.img 
+     key={`img-${animation}`}
+     initial={{ opacity: 0, x:100 }}
+     animate={{ opacity: 1, x: 0 }}
+     transition={{ duration: 1.5,delay: 0.5}}
+      className="img1 w-full max-w-sm mx-auto rounded-lg shadow-lg" 
+      src={enfoque2} 
       alt="Imagen principal"
     />
   </picture>
+  </SwiperSlide>
+  {/* motion2 */}
+  <SwiperSlide>
+  <div className="container mx-auto text-center">
+  <motion.h2
+   key={`h2-${animation}`}
+initial={{ opacity: 0, x: 100 }} 
+animate={{ opacity: 1, x: 0 }}     
+  transition={{ duration: 1.5,delay: 0.5}}l
+  className="title-name text-4xl font-bold text-white"
+>
+  Aplicaciones A Medidas
+</motion.h2>
+
+    <motion.p 
+     key={`p-${animation}`}  initial={{ opacity: 0, x:100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 1.5,delay: 0.5 }} className="subtitle text-lg text-gray-300 mt-2">
+        Sitios web responsivos para cualquier dispositivo, optimizados para una experiencia fluida y eficiente.
+    </motion.p>
+
+
+  
+
+
+  </div>
+
+
+  <picture className="imagenes-container mt-6">
+    <source 
+      srcSet={imgresponsive}
+      media="(max-width:639px)" 
+    />
+    <source 
+      srcSet={imgresponsive} 
+      media="(min-width:640px)" 
+    />
+    <motion.img 
+     key={`img-${animation}`}
+     initial={{ opacity: 0, x:100 }}
+     animate={{ opacity: 1, x: 0 }}
+     transition={{ duration: 1.5,delay: 0.5}}
+      className="img-main w-full max-w-sm mx-auto rounded-lg shadow-lg" 
+      src={imgresponsive}
+      alt="Imagen principal"
+    />
+  </picture>
+  </SwiperSlide>
+  <SwiperSlide>
+  <div className="container mx-auto text-center">
+  <motion.h2
+   key={`h2-${animation}`}
+initial={{ opacity: 0, x: 100 }} 
+animate={{ opacity: 1, x: 0 }}     
+  transition={{ duration: 1.5,delay: 0.5}}l
+  className="title-name text-4xl font-bold text-white"
+>
+  Enfoque Creativo
+</motion.h2>
+
+    <motion.p 
+     key={`p-${animation}`}  initial={{ opacity: 0, x:100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 1.5,delay: 0.5 }} className="subtitle text-lg text-gray-300 mt-2">
+        Diseños Unicos para cualquier proyecto, diseñados con pasión y precisión.
+    </motion.p>
+
+   <motion.button   key={`button-${animation}`}  initial={{ opacity: 0, x:100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 1.5,delay: 0.5 }} className="btn-carrusel">Ver Proyectos</motion.button>
+  
+
+
+  </div>
+
+
+  <picture className="imagenes-container mt-6">
+    <source 
+      srcSet={enfoque}
+      media="(max-width:639px)" 
+    />
+    <source 
+      srcSet={enfoque} 
+      media="(min-width:640px)" 
+    />
+    <motion.img 
+     key={`img-${animation}`}
+     initial={{ opacity: 0, x:100 }}
+     animate={{ opacity: 1, x: 0 }}
+     transition={{ duration: 1.5,delay: 0.5}}
+      className="img-main w-full max-w-sm mx-auto rounded-lg shadow-lg" 
+      src={enfoque}
+      alt="Imagen principal"
+    />
+  </picture>
+  </SwiperSlide>
+  </Swiper>
 </section>
     </>
  )
