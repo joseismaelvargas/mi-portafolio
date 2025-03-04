df02250465853820c8d9d513eb3bb7997e105f4a
ismael <joseismaelvargas@users.noreply.github.com>
2025-03-04 16:37:10 -0300

    se areglo el form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -29,56 +29,88 @@ const Formulario = () => {
       );
   }
   return (
+    <>
+     <h3 className="contactame">Contac <strong className="text-red-700">Me</strong></h3>
     <section className="section-form my-10" id="Contacto" >
+          
       <div className="div-form-text">
-      <h2 className="footer-title">Disponible para oportunidades independientes seleccionadas</h2>
-                  <p className="text-footer">¿Tienes un proyecto interesante con el que necesitas ayuda? ¡
-                Envíame un correo electrónico o contáctame por mensaje instantáneo!</p>
-               <h3 className="copiriht">i00934562@gmail.com</h3>
+        <div className="div-2">
+     
+             <h2 className="form-subtitle">Disponible para oportunidades independientes seleccionadas</h2>
+                  <p className="text-form">¿Tienes un proyecto interesante con el que necesitas ayuda? ¡
+                Envíame un correo electrónico o contáctame por mensaje instantáneo!</p>    
+                   <h3 className="gmail">i00934562@gmail.com</h3>
+        </div>
+   
+        
       </div>
-
+   
     <div className="div-form">
-            <h1 className="title-contacto  text-white ">Contacto...</h1>
+       
          
       <form ref={form} onSubmit={handleSubmit(Sendemail)}>
-        <label htmlFor="">Nombre Completo</label><br />
+       <div className="div-forminput">
+          <label htmlFor="">Nombre Completo</label>
         <input type="text"  {...register("nombre",
          {
           required:"Agregue Su nombre",
           minLength:{value:4,message:"Agregue un Nombre Valido"}
          }
-        )} /><br  />
+        )} />
+       
         {errors.nombre&&<p className="error"> {errors.nombre.message}</p>}
-        <label htmlFor="">Correo Electronico</label><br />
-        <input type="email"  {...register("correo",
+       </div >
+           <div className="div-forminput">
+            <label htmlFor="">Correo Electronico</label>
+               <input type="email"  {...register("correo",
          {
           required:"Agregue Su Correo Electronico",
           minLength:{value:6,message:"Agregue un Correo Valido"}
          }
-        )}/><br />
+        )}/>
+      
          {errors.correo&&<p className="error"> {errors.correo.message}</p>}
-        <label htmlFor="">Asunto</label><br />
+           </div>
+        
+         <div className="div-forminput"> <label htmlFor="">Asunto</label>
         <input type="text"  {...register("asunto",
          {
           required:"Agregue Un Asunto",
           minLength:{value:7,message:"Agregue un Asunto Valido"}
          }
-        )}/><br />
-         {errors.asunto&&<p className="error"> {errors.asunto.message}</p>}
-        <label htmlFor="">Mensaje</label><br />
+        )}/>
+         {errors.asunto&&<p className="error"> {errors.asunto.message}</p>} </div>
+         <div className="div-forminput"> <label htmlFor="">Numero de Telefono</label>
+        <input type="number"  {...register("numero",
+         {
+          required:"Agregue Un Asunto",
+          minLength:{value:3,message:"Agregue un numero Valido"}
+         }
+        )}/>
+         {errors.numero&&<p className="error"> {errors.numero.message}</p>} </div>
+
+        <div className="div-forminput">
+ <label htmlFor="">Mensaje</label><br />
         <textarea type="text"
          {...register("mensaje",
           {
            required:"Agregue Su Mensaje",
            minLength:{value:5,message:"Agregue mas Contexto al Mensaje"}
           }
-         )}></textarea><br />
+         )}></textarea>
           {errors.mesaje&&<p className="error"> {errors.mensaje.message}</p>}
-        
-        <button type="submit">Enviar Mensaje </button>
+        </div>
+       
+       
+         
+            <div className="div-forminput">
+               <button type="submit">Enviar Mensaje </button> 
+            </div>
+   
+   
       </form>
     </div>
-    </section>
+    </section></>
   )
 }
 
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,10 +4,10 @@ export const Footer=()=>{
     return(
         <>
              <footer className="footer">
-                <h2 className="footer-title">Disponible para oportunidades independientes seleccionadas</h2>
+                {/* <h2 className="footer-title">Disponible para oportunidades independientes seleccionadas</h2>
                 <p className="text-footer">¿Tienes un proyecto interesante con el que necesitas ayuda? ¡
                 Envíame un correo electrónico o contáctame por mensaje instantáneo!</p>
-               <h3 className="copiriht">i00934562@gmail.com</h3>
+               <h3 className="copiriht">i00934562@gmail.com</h3> */}
         
              <Link className="list" to={"https://github.com/joseismaelvargas"} >Github</Link>
             <Link className="list" to={"https://www.linkedin.com/in/ismael-jose-vargas-221118351/"}>Linkedin</Link>
